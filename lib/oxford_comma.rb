544faa9f2d7ca81b3ec86807ef9e846878981123
poignant-minifier-0983 <davidfano@gmail.com>
2019-05-13 22:21:33 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,15 +1,10 @@
 def oxford_comma(arr)
-
-  if arr.count == 1
-    arr.join
-  end
-
-  if arr.count == 2
-    arr.join(" and ")
-  end
-
   arr.each_with_index.map do |item, index|
-		if index < arr.count-2
+    if arr.count == 1
+      arr.join
+    elsif arr.count == 2
+      arr.join("and")    
+		elsif index < arr.count-2
 			item << ", "
 		elsif index == arr.count-2
 			item << ", and "
